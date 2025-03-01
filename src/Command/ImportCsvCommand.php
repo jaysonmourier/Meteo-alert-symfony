8276a0f16b063479a7bee79c8d4d4db710ba77ef
Jayson Mourier <jayson.mourier@proton.me>
2025-03-01 15:13:07 +0100

    refactoring

--- src/Command/ImportCsvCommand.php
+++ src/Command/ImportCsvCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Command;
 
+use Exception;
 use Doctrine\DBAL\Connection;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
@@ -14,35 +15,42 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
     name: 'app:csv-import',
-    description: 'Import a CSV',
+    description: 'Import a CSV file, parse its content, and store valid (INSEE, PHONE) records in the database.',
     hidden: false
 )]
 class ImportCsvCommand extends Command 
 {
+    public const ARG_FILE_PATH = "file";
     public const INSEE_INDEX = "insee";
     public const TELEPHONE_INDEX = "telephone";
 
     private int $rowNumber;
     private int $successCount;
-    private int $errorCount;
+    private int $errorsCount;
 
     public function __construct(
         private Connection $connection,
-        private LoggerInterface $loggerInterface
+        private LoggerInterface $logger
     ) {
         parent::__construct();
         $this->rowNumber = 0;
         $this->successCount = 0;
-        $this->errorCount = 0;
+        $this->errorsCount = 0;
     }
 
     protected function configure(): void {
-        $this->addArgument('file', InputArgument::REQUIRED, 'Path to the CSV file');
+        $this->addArgument(self::ARG_FILE_PATH, InputArgument::REQUIRED, 'Path to the CSV file');
     }
 
     /**
-     * Fonction principale de la commande, elle permet d'appeler les fonctions nécessaires
-     * au bon fonctionnement de la commande
+     * Méthode principale de la commande app:csv-import, elle fonctionne comme tel:
+     *  - Elle récupère le chemin du fichier avec la méthode getArgument
+     *  - Elle vérifie l'existence du fichier
+     *  - Elle parse le fichier CSV via la méthode parseCsvRows
+     *  - Elle insert les données en base via la méthode dataPersist
+     *  - Elle affiche le rapport pour informer l'utilisateur du nombre de succès et d'erreurs
+     * 
+     * En cas de succès, la méthode retourne Command::SUCCESS. En cas d'erreur, Command::FAILURE est retourné.
      * 
      * @param \Symfony\Component\Console\Input\InputInterface $inputInterface
      * @param \Symfony\Component\Console\Output\OutputInterface $outputInterface
@@ -51,7 +59,7 @@ class ImportCsvCommand extends Command
     protected function execute(InputInterface $inputInterface, OutputInterface $outputInterface): int {
         try {
             // get file path
-            $filePath = $inputInterface->getArgument('file');
+            $filePath = $inputInterface->getArgument(self::ARG_FILE_PATH);
 
             // check if file exists
             if (!file_exists($filePath)) {
@@ -60,60 +68,66 @@ class ImportCsvCommand extends Command
             }
 
             // parse CSV
-            $batchData = $this->parseCsvRows($filePath);
-            
-            // persist data to database
-            $this->batchInsert($batchData);
+            $validRows = $this->parseCsvRows($filePath);
+            if (!empty($validRows)) {
+                $this->dataPersist($validRows);
+            }
 
             // print report
             $this->printReport($outputInterface);
 
             return Command::SUCCESS;
-        } catch (\RuntimeException $e) {
+        } catch (Exception $e) {
             $outputInterface->writeln('<error>' . $e->getMessage() . '</error>');
             return Command::FAILURE;
         }
     }
 
     /**
-     * Vérifie que le numéro INSEE est bien valide
-     * Si il est valide, isValidInsee retourne true sinon, false
+     * isValidInsee permet de vérifier que le code INSEE passé en paramètre est bien un code INSEE valide.
+     * Si le code INSEE est valide, la méthode retourne 'true'. Sinon, elle retourne 'false'.
      * 
      * @param string $insee
-     * @return bool|int
+     * @return bool
      */
     private function isValidInsee(string $insee): bool
     {
-        return $insee !== null && preg_match('/^\d{5}$/', $insee);
+        return $insee !== null && (bool)preg_match('/^\d{5}$/', $insee);
     }
 
     /**
-     * Vérifie que le numéro de téléphone soit bien valide
-     * Si il est valide, isValidPhone retourne true sinon, false
+     * isValidPhone permet de vérifier que le numéro de téléphone passé en paramètre est bien un numéro valide.
+     * Si le numéro est valide, la méthode retourne 'true'. Sinon, elle retourne 'false'.
      * 
      * @param string $phone
-     * @return bool|int
+     * @return bool
      */
     private function isValidPhone(string $phone): bool
     {
-        return $phone !== null && preg_match('/^\+?\d{10,15}$/', $phone);
+        return $phone !== null && (bool)preg_match('/^\+?\d{10,15}$/', $phone);
     }
 
     /**
-     * Cette fonction permet d'ouvrir et de parcourir un CSV 
-     * afin de créer un batch de données valides.
+     * Cette méthode permet d'ouvrir le fichier CSV situé au chemin $filePath donné en paramètre,
+     * d'ouvrir et parcourir ligne par ligne le fichier CSV afin d'en extraire les lignes valides.
      * 
-     * Nous considérons valide un couple (INSEE, TELEPHONE) 
-     * si les fonctions isValidInsee et isValidPhone retournent vrai
-     * pour chacun des paramètres respectifs
+     * Une ligne est considérée comme valide si le couple (INSEE, TELEPHONE) extrait, satisfait bien les 
+     * méthodes isValidInsee et isValidPhone.
+     * 
+     * Si aucun couple (INSEE, TELEPHONE) n'est valide, alors la fonction retourne un tableau vide
+     * 
+     * Cette méthode fonctionne comme tel: 
+     * - Elle ouvre le fichier relatif au chemin $filePath. En cas d'erreur, une exception est levée.
+     * - Elle boucle avec comme condition de sortie, le retour de la fonction fgetcsv.
+     * - Pour chaque ligne, parseCsvRows vérifie que le code INSEE et le numéro de téléphone extrait soit bien valide.
+     * - Si le couple (INSEE, TELEPHONE) est valide, elle l'ajoute au tableau des couples valides.
+     * - Une fois terminé, la fonction retourne le tableau avec tous les couples valides du fichier CSV.
      * 
      * @param string $filePath
-     * @param \Symfony\Component\Console\Output\OutputInterface $outputInterface
      * @throws \RuntimeException
-     * @return array[]
+     * @return array
      */
     private function parseCsvRows(string $filePath): array {
-        // open file
         $handle = fopen($filePath, 'r');
 
         if ($handle === false) {
@@ -125,22 +139,22 @@ class ImportCsvCommand extends Command
             $this->rowNumber++;
 
             if (count($row) < 2) {
-                $this->loggerInterface->info('Skipping invalid row #' . $this->rowNumber);
-                $this->errorCount++;
+                $this->errorsCount++;
+                $this->logger->info('Skipping invalid row #' . $this->rowNumber);
                 continue;
             }
             
             [$insee, $telephone] = $row;
             
             if (!$this->isValidInsee($insee) || !$this->isValidPhone($telephone)) {
-                $this->loggerInterface->info('Invalid data at row #' . $this->rowNumber);
-                $this->errorCount++;
+                $this->errorsCount++;
+                $this->logger->info('Invalid data at row #' . $this->rowNumber);
                 continue;
             }
             
             $batchData[] = [self::INSEE_INDEX => $insee, self::TELEPHONE_INDEX => $telephone];
             $this->successCount++;
-            $this->loggerInterface->info("Row #" . $this->rowNumber . ": INSEE=" . $insee . ", PHONE:" . $telephone);
+            $this->logger->info("Row #" . $this->rowNumber . ": INSEE=" . $insee . ", PHONE:" . $telephone);
         }
 
         fclose($handle);
@@ -149,20 +163,33 @@ class ImportCsvCommand extends Command
     }
 
     /**
-     * batchInsert permet de sauvegarder efficacement des données en base de données.
-     * L'idée est d'envoyer les données sous forme de chunks afin de minimiser les requêtes SQL.
+     * La méthode dataPersist permet de persister efficacement les données en base de données.
+     * Elle repose sur un système de chunks. Par défaut, la taille d'un chunk est de 100 éléments.
+     * L'objectif est de minimiser le nombre de requêtes faites vers la base de données.
+     * 
+     * La méthode peut lever une exception si 'executeStatement' échoue
+     * 
+     * La méthode fonctionne comme tel:
+     * - Elle découpe le tableau en plusieurs chunks de taille $chunkSize
+     * - Pour chacun des éléments des chunks :
+     *      . on génère son placeholder (?, ?) pour l'injecter dans la requête SQL
+     *      . on ajoute son code INSEE au tableau 'values'
+     *      . on ajoute son numéro de téléphone au tableau 'values'
+     *      . on injecte le placeholder est le tableau 'values' dans une requête SQL
+     *      . on exécute la requête via la méthode executeStatement
+     *  
      * 
      * @param array $data
-     * @param int $batchSize
-     * @throws \RuntimeException
+     * @param int $chunkSize
+     * @throws Exception
      * @return void
      */
-    private function batchInsert(array $data, int $batchSize = 100): void {
+    private function dataPersist(array $data, int $chunkSize = 100): void {
         if (empty($data)) {
-            throw new \RuntimeException("data is empty, nothing to insert");
+            return;
         }
 
-        $chunks = array_chunk($data, $batchSize);
+        $chunks = array_chunk($data, $chunkSize);
 
         foreach ($chunks as $chunk) {
             $placeholders = [];
@@ -178,13 +205,22 @@ class ImportCsvCommand extends Command
             . implode(", ", $placeholders) . 
             " ON CONFLICT (telephone) DO NOTHING";
             
-            $this->connection->executeStatement($sql, $values);
+            try {
+                $this->connection->executeStatement($sql, $values);
+            } catch (Exception $e) {
+                throw $e;
+            }
         }
     }
 
     /**
-     * printReport permet d'afficher un rapport de l'exécution de la commande app:csv-import
-     * Elle prend en paramètre le nombre de succès et le nombre d'erreurs
+     * La méthode printReport permet de générer un rapport pour informer l'utilisateur
+     * sur la finalité de l'exécution de la commande app:csv-import.
+     * 
+     * Elle affiche:
+     *  - le nombre total de lignes lues
+     *  - Le nombre total de lignes correctement parsées
+     *  - Le nombre total d'erreurs
      * 
      * @param \Symfony\Component\Console\Output\OutputInterface $outputInterface
      * @return void
@@ -195,9 +231,9 @@ class ImportCsvCommand extends Command
 
         $outputInterface->writeln("<info>Total rows:</info>  " . $this->rowNumber);
         $outputInterface->writeln("<info>✔ Success Count:</info>  " . $this->successCount);
-        $outputInterface->writeln("<error>✖ Error Count:</error>  " . $this->errorCount);
+        $outputInterface->writeln("<error>✖ Error Count:</error>  " . $this->errorsCount);
 
-        if ($this->errorCount > 0) {
+        if ($this->errorsCount > 0) {
             $outputInterface->writeln("\n<comment>⚠ Some rows were skipped due to errors.</comment>");
         }
 
