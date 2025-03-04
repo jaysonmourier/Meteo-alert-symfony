6c68c9d98f45c5b99c5cf9a8c24e783e9ce31a17
Jayson Mourier <jayson.mourier@proton.me>
2025-03-04 15:23:08 +0100

    add ImportReportService

--- src/Command/ImportCsvCommand.php
+++ src/Command/ImportCsvCommand.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 namespace App\Command;
 
 use Exception;
-use App\Dto\CsvParseResult;
 use Psr\Log\LoggerInterface;
 use App\Service\CsvParserService;
 use App\Service\DataValidatorService;
 use App\Repository\DestinataireRepository;
+use App\Service\ImportReportService;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -25,16 +25,12 @@ class ImportCsvCommand extends Command
 {
     public const ARG_FILE_PATH = "file";
 
-    private int $rowNumber;
-    private int $successCount;
-    private int $errorsCount;
-    private int $insertedRows;
-
     public function __construct(
         private LoggerInterface $logger,
         private DestinataireRepository $destinataireRepository,
         private DataValidatorService $dataValidatorService,
-        private CsvParserService $csvParserService
+        private CsvParserService $csvParserService,
+        private ImportReportService $importReportService
     ) {
         parent::__construct();
     }
@@ -62,13 +58,20 @@ class ImportCsvCommand extends Command
             // get file path
             $filePath = $inputInterface->getArgument(self::ARG_FILE_PATH);
 
+            // parse csv
             $csvParseResult = $this->csvParserService->parse($filePath);
             if (!empty($csvParseResult->data)) {
-                $this->insertedRows = $this->destinataireRepository->insertBulk($$csvParseResult->data);
+                $insertedRows = $this->destinataireRepository->insertBulk($csvParseResult->data);
             }
 
-            // print report
-            $this->printReport($outputInterface);
+            // generate report
+            $this->importReportService->generate(
+                $outputInterface,
+                $csvParseResult->totalRows,
+                $csvParseResult->totalValidRows,
+                $insertedRows, 
+                $csvParseResult->totalErrorRows, 
+            );
 
             return Command::SUCCESS;
         } catch (Exception $e) {
@@ -76,33 +79,5 @@ class ImportCsvCommand extends Command
             $this->logger->error("Exception: " . $e->getMessage());
             return Command::FAILURE;
         }
-    }
-
-    /**
-     * La méthode printReport permet de générer un rapport pour informer l'utilisateur
-     * sur la finalité de l'exécution de la commande app:csv-import.
-     * 
-     * Elle affiche:
-     *  - le nombre total de lignes lues
-     *  - Le nombre total de lignes correctement parsées
-     *  - Le nombre total d'erreurs
-     * 
-     * @param \Symfony\Component\Console\Output\OutputInterface $outputInterface
-     * @return void
-     */
-    private function printReport(OutputInterface $outputInterface): void {
-        $outputInterface->writeln("\n<info>CSV IMPORT REPORT</info>");
-        $outputInterface->writeln("<comment>===================</comment>\n");
-
-        $outputInterface->writeln("<info>Total rows:</info>  " . $this->rowNumber);
-        $outputInterface->writeln("<info>✔ Success Count:</info>  " . $this->successCount);
-        $outputInterface->writeln("<info>✔ Inserted Rows:</info>  " . $this->insertedRows);
-        $outputInterface->writeln("<error>✖ Error Count:</error>  " . $this->errorsCount);
-
-        if ($this->errorsCount > 0) {
-            $outputInterface->writeln("\n<comment>⚠ Some rows were skipped due to errors.</comment>");
-        }
-
-        $outputInterface->writeln("\n<comment>===================</comment>\n");
     }
 }
---
+++ src/Service/ImportReportService.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ImportReportService
+{
+    /**
+     * La méthode generate permet de générer un rapport pour informer l'utilisateur
+     * sur la finalité de l'exécution de la commande app:csv-import.
+     * 
+     * Elle affiche:
+     *  - le nombre total de lignes lues
+     *  - Le nombre total de lignes correctement parsées
+     *  - Le nombre total de lignes insérées en base
+     *  - Le nombre total d'erreurs
+     * 
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @param int $totalRows
+     * @param int $totalSuccessRows
+     * @param int $totalInsertedRows
+     * @param int $totalErrorRows
+     * @return void
+     */
+    public function generate(
+        OutputInterface $output, 
+        int $totalRows, 
+        int $totalSuccessRows, 
+        int $totalInsertedRows, 
+        int $totalErrorRows): void {
+        $output->writeln("\n<info>CSV IMPORT REPORT</info>");
+        $output->writeln("<comment>===================</comment>\n");
+    
+        $output->writeln("<info>Total rows:</info>  " . $totalRows);
+        $output->writeln("<info>✔ Success Count:</info>  " . $totalSuccessRows);
+        $output->writeln("<info>✔ Inserted Rows:</info>  " . $totalInsertedRows);
+        $output->writeln("<error>✖ Error Count:</error>  " . $totalErrorRows);
+    
+        if ($totalErrorRows > 0) {
+            $output->writeln("\n<comment>⚠ Some rows were skipped due to errors.</comment>");
+        }
+    
+        $output->writeln("\n<comment>===================</comment>\n");
+    }
+}
